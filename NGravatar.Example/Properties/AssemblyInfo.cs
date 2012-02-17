88aad7bef0f027f0b16dbfceb86478a3c2dd0c04
kyourek <kyourek@gmail.com>
2012-02-17 04:20:11 +0000

    More minor updates.

--- NGravatar.Example/Properties/AssemblyInfo.cs
+++ NGravatar.Example/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NGravatar.Example")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("NGravatar Example")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NGravatar.Example")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyCopyright("Copyright © Ken Yourek 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
