5581be5472d0182fd3e8d2092e6a8d418e06a8cf
kyourek@gmail.com <kyourek@gmail.com@f3f85ded-8c0c-14a0-d2d9-98d2ba1cfc74>
2012-03-03 08:49:27 +0000

    Added more Gravatar profile functionality and tests.

---
+++ branches/ngravatar.0.3.0/NGravatar.Tests/GrofileInfoTests.cs
@@ -0,0 +1,202 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using System.Text;
+
+namespace NGravatar.Tests
+{
+    [TestFixture]
+    public class XElementExtensionsTests
+    {
+        [Test]
+        public void ElementValueOrDefaultTest()
+        {
+            var xml = "<xel><el1>el1text</el1></xel>";
+            var xel = XElement.Parse(xml);
+            Assert.AreEqual("el1text", xel.ElementValueOrDefault("el1", null));
+            Assert.AreEqual(null, xel.ElementValueOrDefault("el2", null));
+        }
+    }
+
+    [TestFixture]
+    public class GrNameTests
+    {
+        [Test]
+        public void ToStringTest()
+        {
+            var formatted = "Some Name";
+            var name = new GrName(formatted, null, null, null, null, null);
+            Assert.AreEqual(formatted, name.ToString());
+        }
+    }
+
+    [TestFixture]
+    public class GrofileInfoTests
+    {
+        [Test]
+        public void IdTest()
+        {
+            var expected = Entry.Element("id").Value;
+            var actual = InfoXml.Id;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void HashTest()
+        {
+            var expected = Entry.Element("hash").Value;
+            var actual = InfoXml.Hash;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void RequestHashTest()
+        {
+            var expected = Entry.Element("requestHash").Value;
+            var actual = InfoXml.RequestHash;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void ProfileUrlTest()
+        {
+            var expected = Entry.Element("profileUrl").Value;
+            var actual = InfoXml.ProfileUrl;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void PreferredUsernameTest()
+        {
+            var expected = Entry.Element("preferredUsername").Value;
+            var actual = InfoXml.PreferredUsername;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void ThumbnailUrlTest()
+        {
+            var expected = Entry.Element("thumbnailUrl").Value;
+            var actual = InfoXml.ThumbnailUrl;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void DisplayNameTest()
+        {
+            var expected = Entry.Element("displayName").Value;
+            var actual = InfoXml.DisplayName;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void AboutMeTest()
+        {
+            var expected = Entry.Element("aboutMe").Value;
+            var actual = InfoXml.AboutMe;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void CurrentLocationTest()
+        {
+            var expected = Entry.Element("currentLocation").Value;
+            var actual = InfoXml.CurrentLocation;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void NameTest()
+        {
+            var formatted = Entry.ElementValueOrDefault("formatted", null);
+            var familyName = Entry.ElementValueOrDefault("familyName", null);
+            var givenName = Entry.ElementValueOrDefault("givenName", null);
+            var middleName = Entry.ElementValueOrDefault("middleName", null);
+            var honorificPrefix = Entry.ElementValueOrDefault("honorificPrefix", null);
+            var honorificSuffix = Entry.ElementValueOrDefault("honorificSuffix", null);
+
+            var name = new GrName(formatted, familyName, givenName, middleName, honorificPrefix, honorificSuffix);
+            var actual = InfoXml.Name;
+
+            Assert.AreEqual(name.FamilyName, actual.FamilyName);
+            Assert.AreEqual(name.Formatted, actual.Formatted);
+            Assert.AreEqual(name.GivenName, actual.GivenName);
+            Assert.AreEqual(name.HonorificPrefix, actual.HonorificPrefix);
+            Assert.AreEqual(name.HonorificSuffix, actual.HonorificSuffix);
+            Assert.AreEqual(name.MiddleName, actual.MiddleName);
+        }
+
+        private GrofileInfoXml InfoXml
+        {
+            get { return new GrofileInfoXml(Entry); }
+        }
+
+        private XElement Entry
+        {
+            get
+            {
+                return XElement.Parse(@"
+<entry>
+	<id>32922842</id>
+	<hash>bccc2b381d103797427c161951be5fa5</hash>
+	<requestHash>ngravatar</requestHash>
+	<profileUrl>http://gravatar.com/ngravatar</profileUrl>
+	<preferredUsername>ngravatar</preferredUsername>
+
+	<thumbnailUrl>http://2.gravatar.com/avatar/bccc2b381d103797427c161951be5fa5</thumbnailUrl>
+	<photos>
+		<value>http://2.gravatar.com/avatar/bccc2b381d103797427c161951be5fa5</value>
+		<type>thumbnail</type>
+	</photos>
+	<photos>
+		<value>http://0.gravatar.com/userimage/32922842/8766872a3b38265fdec89a08843001c3</value>
+
+	</photos>
+	<photos>
+		<value>http://2.gravatar.com/userimage/32922842/c3c5baa63482fd02d34d1aa20e67b4e2</value>
+	</photos>
+	<name>
+		<givenName>N</givenName>
+		<familyName>Gravatar</familyName>
+
+		<formatted>NGravatar</formatted>
+	</name>
+	<displayName>ngravatar</displayName>
+	<aboutMe>NGravatar is a .NET library for getting profile and avatar information from gravatar.com.</aboutMe>
+	<currentLocation>Chicago</currentLocation>
+	<emails>
+		<value>ngravatar@kendoll.net</value>
+
+		<primary>true</primary>
+	</emails>
+	<accounts>
+		<domain>twitter.com</domain>
+		<username>kendollDotNet</username>
+		<display>@kendollDotNet</display>
+		<url>http://twitter.com/kendollDotNet</url>
+
+		<verified>true</verified>
+		<shortname>twitter</shortname>
+	</accounts>
+	<urls>
+		<title>kendoll.net</title>
+		<value>http://kendoll.net</value>
+	</urls>
+
+	<urls>
+		<title>Google Code</title>
+		<value>http://code.google.com/p/ngravatar/</value>
+	</urls>
+	<urls>
+		<title>NuGet</title>
+		<value>http://nuget.org/packages/NGravatar</value>
+
+	</urls>
+</entry>
+                ");
+            }
+        }
+    }
+}
--- branches/ngravatar.0.3.0/NGravatar/GrofileInfo.cs
+++ branches/ngravatar.0.3.0/NGravatar/GrofileInfo.cs
@@ -6,17 +6,121 @@ using System.Xml.Linq;
 
 namespace NGravatar
 {
-    public abstract class GrofileInfo
+    static class XElementExtensions
     {
+        public static string ElementValueOrDefault(this XElement xElement, string elementName, string defaultValue)
+        {
+            if (null == xElement) throw new ArgumentNullException("xElement");
+            
+            var element = xElement.Element(elementName);
+            if (element == null) return defaultValue;
+            return element.Value;
+        }
     }
 
-    internal class GrofileInfoXml : GrofileInfo
+    public class GrName
     {
-        private readonly XDocument _XDocument;
+        /// <summary>
+        /// Gets the full name, including all middle names, titles, and suffixes as appropriate, formatted for display (e.g. Mr. Joseph Robert Smarr, Esq.). This is the Primary Sub-Field for this field, for the purposes of sorting and filtering. 
+        /// </summary>
+        public string Formatted { get; private set; }
 
-        public GrofileInfoXml(XDocument xDocument)
+        /// <summary>
+        /// Gets the family name of this Contact, or "Last Name" in most Western languages (e.g. Smarr given the full name Mr. Joseph Robert Smarr, Esq.). 
+        /// </summary>
+        public string FamilyName { get; private set; }
+
+        /// <summary>
+        /// Gets the given name of this Contact, or "First Name" in most Western languages (e.g. Joseph given the full name Mr. Joseph Robert Smarr, Esq.). 
+        /// </summary>
+        public string GivenName { get; private set; }
+
+        /// <summary>
+        /// Gets the middle name(s) of this Contact (e.g. Robert given the full name Mr. Joseph Robert Smarr, Esq.). 
+        /// </summary>
+        public string MiddleName { get; private set; }
+
+        /// <summary>
+        /// Gets the honorific prefix(es) of this Contact, or "Title" in most Western languages (e.g. Mr. given the full name Mr. Joseph Robert Smarr, Esq.). 
+        /// </summary>
+        public string HonorificPrefix { get; private set; }
+
+        /// <summary>
+        /// Gets the honorifix suffix(es) of this Contact, or "Suffix" in most Western languages (e.g. Esq. given the full name Mr. Joseph Robert Smarr, Esq.). 
+        /// </summary>
+        public string HonorificSuffix { get; private set; }
+
+        public GrName(string formatted, string familyName, string givenName, string middleName, string honorificPrefix, string honorificSuffix)
         {
-            if (null == xDocument) throw new ArgumentNullException("xDocument");
+            Formatted = formatted;
+            FamilyName = familyName;
+            GivenName = givenName;
+            MiddleName = middleName;
+            HonorificPrefix = honorificPrefix;
+            HonorificSuffix = honorificSuffix;
+        }
+
+        public override string ToString()
+        {
+            return Formatted;
+        }
+    }
+
+    public interface IGrofileInfo
+    {
+        string Id { get; }
+        string Hash { get; }
+        string RequestHash { get; }
+        string ProfileUrl { get; }
+        string PreferredUsername { get; }
+    }
+
+    internal class GrofileInfoXml : IGrofileInfo
+    {
+        private readonly XElement _Entry;
+
+        private GrName GetGrName()
+        {
+            var formatted = Entry.ElementValueOrDefault("formatted", null);
+            var familyName = Entry.ElementValueOrDefault("familyName", null);
+            var givenName = Entry.ElementValueOrDefault("givenName", null);
+            var middleName = Entry.ElementValueOrDefault("middleName", null);
+            var honorificPrefix = Entry.ElementValueOrDefault("honorificPrefix", null);
+            var honorificSuffix = Entry.ElementValueOrDefault("honorificSuffix", null);
+
+            return new GrName(formatted, familyName, givenName, middleName, honorificPrefix, honorificSuffix);
+        }
+
+        public XElement Entry { get { return _Entry; } }
+
+        public string Id { get; private set; }
+        public string Hash { get; private set; }
+        public string RequestHash { get; private set; }
+        public string ProfileUrl { get; private set; }
+        public string PreferredUsername { get; private set; }
+        public string ThumbnailUrl { get; private set; }
+        public string DisplayName { get; private set; }
+        public string AboutMe { get; private set; }
+        public string CurrentLocation { get; private set; }
+
+        public GrName Name { get; private set; }
+
+        public GrofileInfoXml(XElement entry)
+        {
+            if (null == entry) throw new ArgumentNullException("entry");
+            _Entry = entry;
+
+            Id = Entry.ElementValueOrDefault("id", null);
+            Hash = Entry.ElementValueOrDefault("hash", null);
+            RequestHash = Entry.ElementValueOrDefault("requestHash", null);
+            ProfileUrl = Entry.ElementValueOrDefault("profileUrl", null);
+            PreferredUsername = Entry.ElementValueOrDefault("preferredUsername", null);
+            ThumbnailUrl = Entry.ElementValueOrDefault("thumbnailUrl", null);
+            DisplayName = Entry.ElementValueOrDefault("displayName", null);
+            AboutMe = Entry.ElementValueOrDefault("aboutMe", null);
+            CurrentLocation = Entry.ElementValueOrDefault("currentLocation", null);
+
+            Name = GetGrName();
         }
     }
 }
