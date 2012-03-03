92c215c32c71d625211a7d99cd57a2d0b7633e91
kyourek@gmail.com <kyourek@gmail.com@f3f85ded-8c0c-14a0-d2d9-98d2ba1cfc74>
2012-03-03 06:29:20 +0000

    Created Gremail email helper to do the hashing. Also added NUnit as a NuGet package.

---
+++ branches/ngravatar.0.3.0/NGravatar.Tests/GremailTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NGravatar.Tests
+{
+    [TestFixture]
+    public class GremailTests
+    {
+        [Test]
+        public void HashTest()
+        {
+            var email = "MyEmailAddress@example.com ";
+            Assert.AreEqual("0bc83cb571cd1c50ba6f3e8a78ef1346", new Gremail(email).Hash());
+        }
+    }
+}
