e9167b241623d0f6d6c8e82e06a43ce119115bff
kyourek <kyourek@gmail.com>
2012-03-07 00:42:40 +0000

    Added documentation for new methods and classes in version 0.3.0.

--- NGravatar/GrofileInfo.cs
+++ NGravatar/GrofileInfo.cs
@@ -18,17 +18,58 @@ namespace NGravatar
         }
     }
 
+    /// <summary>
+    /// A field of a Gravatar profile that may have more than one instance per profile.
+    /// </summary>
     public class GrofilePluralField
     {
+        /// <summary>
+        /// Gets the value of the field.
+        /// </summary>
         public string Value { get; private set; }
+
+        /// <summary>
+        /// Gets the type of the field.
+        /// </summary>
         public string Type { get; private set; }
+
+        /// <summary>
+        /// Gets a flag indicating whether or not this field is the primary instance
+        /// of all other similar fields.
+        /// </summary>
         public bool Primary { get; private set; }
 
+        /// <summary>
+        /// Creates a new instance of the field.
+        /// </summary>
         public GrofilePluralField() : this(null) { }
+
+        /// <summary>
+        /// Creates a new instance of the field.
+        /// </summary>
+        /// <param name="value">The value of the field.</param>
         public GrofilePluralField(string value) : this(value, null) { }
+
+        /// <summary>
+        /// Creates a new instance of the Gravatar profile field.
+        /// </summary>
+        /// <param name="value">The value of the field.</param>
+        /// <param name="type">The type of the field.</param>
         public GrofilePluralField(string value, string type) : this(value, type, false) { }
+
+        /// <summary>
+        /// Creates a new instance of the Gravatar profile field.
+        /// </summary>
+        /// <param name="value">The value of the field.</param>
+        /// <param name="primary">A flag indicating whether or not this field is the primary instance of all other similar fields.</param>
         public GrofilePluralField(string value, bool primary) : this(value, null, primary) { }
 
+        /// <summary>
+        /// Creates a new instance of the Gravatar profile field.
+        /// </summary>
+        /// <param name="value">The value of the field.</param>
+        /// <param name="type">The type of the field.</param>
+        /// <param name="primary">A flag indicating whether or not this field is the primary instance of all other similar fields.</param>
         public GrofilePluralField(string value, string type, bool primary)
         {
             Value = value;
@@ -36,43 +77,119 @@ namespace NGravatar
             Primary = primary;
         }
 
+        /// <summary>
+        /// Gets a string representation of the field.
+        /// </summary>
+        /// <returns>The field value as a string.</returns>
         public override string ToString()
         {
             return Value;
         }
     }
 
+    /// <summary>
+    /// A URL field in a Gravatar profile.
+    /// </summary>
     public class GrofileUrl : GrofilePluralField
     {
+        /// <summary>
+        /// Gest the title of the URL.
+        /// </summary>
         public string Title { get; private set; }
 
+        /// <summary>
+        /// Creates a new instance of the URL Gravatar profile field.
+        /// </summary>
+        /// <param name="title">The title of the URL.</param>
+        /// <param name="value">The URL value.</param>
         public GrofileUrl(string title, string value) : base(value)
         {
             Title = title;
         }
     }
 
+    /// <summary>
+    /// An email field in a Gravatar profile.
+    /// </summary>
     public class GrofileEmail : GrofilePluralField
     {
+        /// <summary>
+        /// Creates a new instance of the email field.
+        /// </summary>
+        /// <param name="value">The email address.</param>
         public GrofileEmail(string value) : base(value, false) { }
+
+        /// <summary>
+        /// Creates a new instance of the email field.
+        /// </summary>
+        /// <param name="value">The email address.</param>
+        /// <param name="primary">A flag indicating whether or not this is the primary email address of the profile.</param>
         public GrofileEmail(string value, bool primary) : base(value, primary) { }
     }
 
+    /// <summary>
+    /// A photo field in a Gravatar profile.
+    /// </summary>
     public class GrofilePhoto : GrofilePluralField
     {
+        /// <summary>
+        /// Creates a new instance of the photo field
+        /// </summary>
+        /// <param name="value">The URI of the photo.</param>
         public GrofilePhoto(string value) : this(value, null) { }
+
+        /// <summary>
+        /// Creates a new instance of the photo field.
+        /// </summary>
+        /// <param name="value">The URI of the photo.</param>
+        /// <param name="type">The type of the photo.</param>
         public GrofilePhoto(string value, string type) : base(value, type) { }
     }
 
+    /// <summary>
+    /// A field in a Gravatar profile that provides information about a user's account.
+    /// </summary>
     public class GrofileAccount : GrofilePluralField
     {
+        /// <summary>
+        /// Gets the domain of this account.
+        /// </summary>
         public string Domain { get; private set; }
+
+        /// <summary>
+        /// Gets the username of this account.
+        /// </summary>
         public string Username { get; private set; }
+
+        /// <summary>
+        /// Gets a display string for this account.
+        /// </summary>
         public string Display { get; private set; }
+
+        /// <summary>
+        /// Gets the URL of this account.
+        /// </summary>
         public string Url { get; private set; }
+
+        /// <summary>
+        /// Gets the short name of this account.
+        /// </summary>
         public string Shortname { get; private set; }
+
+        /// <summary>
+        /// Gets a flag indicating whether or not this account has been verified.
+        /// </summary>
         public bool Verified { get; private set; }
 
+        /// <summary>
+        /// Creates a new instance of a Gravatar profile account.
+        /// </summary>
+        /// <param name="domain">The domain of the account.</param>
+        /// <param name="username">The account username.</param>
+        /// <param name="display">The string to display for the account.</param>
+        /// <param name="url">The URL of the account.</param>
+        /// <param name="shortname">The short name of the account.</param>
+        /// <param name="verified">A flag indicating whether or not the account is verified.</param>
         public GrofileAccount(string domain, string username, string display, string url, string shortname, bool verified)
         {
             Domain = domain;
@@ -83,12 +200,19 @@ namespace NGravatar
             Verified = verified;
         }
 
+        /// <summary>
+        /// Gets a string representation of the account.
+        /// </summary>
+        /// <returns>The account string to display.</returns>
         public override string ToString()
         {
             return Display;
         }
     }
 
+    /// <summary>
+    /// The name field of a Gravatar profile.
+    /// </summary>
     public class GrofileName
     {
         /// <summary>
@@ -121,6 +245,15 @@ namespace NGravatar
         /// </summary>
         public string HonorificSuffix { get; private set; }
 
+        /// <summary>
+        /// Creates a new instance of the Gravatar profile name.
+        /// </summary>
+        /// <param name="formatted">The full formatted name.</param>
+        /// <param name="familyName">The family name or surname.</param>
+        /// <param name="givenName">The given name or first name.</param>
+        /// <param name="middleName">The middle name.</param>
+        /// <param name="honorificPrefix">The prefix of the name.</param>
+        /// <param name="honorificSuffix">The suffix of the name.</param>
         public GrofileName(string formatted, string familyName, string givenName, string middleName, string honorificPrefix, string honorificSuffix)
         {
             Formatted = formatted;
@@ -131,28 +264,89 @@ namespace NGravatar
             HonorificSuffix = honorificSuffix;
         }
 
+        /// <summary>
+        /// Gets the name formatted as a string.
+        /// </summary>
+        /// <returns>The string formatted name.</returns>
         public override string ToString()
         {
             return Formatted;
         }
     }
 
+    /// <summary>
+    /// Interface for Gravatar profile information objects.
+    /// </summary>
     public interface IGrofileInfo
     {
+        /// <summary>
+        /// Gets the ID value of this profile.
+        /// </summary>
         string Id { get; }
+
+        /// <summary>
+        /// Gets this profile hash.
+        /// </summary>
         string Hash { get; }
+
+        /// <summary>
+        /// Gets this profile request hash.
+        /// </summary>
         string RequestHash { get; }
+
+        /// <summary>
+        /// Gets the URL of this profile.
+        /// </summary>
         string ProfileUrl { get; }
+
+        /// <summary>
+        /// Gets the preferred username of this profile's owner.
+        /// </summary>
         string PreferredUsername { get; }
+
+        /// <summary>
+        /// Gets the URL of the thumbnail for this profile.
+        /// </summary>
         string ThumbnailUrl { get; }
+
+        /// <summary>
+        /// Gets the name to display for the owner of this profile.
+        /// </summary>
         string DisplayName { get; }
+
+        /// <summary>
+        /// Gets information about the owner of this profile.
+        /// </summary>
         string AboutMe { get; }
+
+        /// <summary>
+        /// Gets the current location value of this profile.
+        /// </summary>
         string CurrentLocation { get; }
 
+        /// <summary>
+        /// Gets the profile name section of this profile.
+        /// </summary>
         GrofileName Name { get; }
+
+        /// <summary>
+        /// Gets a collection of URLs associated with this profile.
+        /// </summary>
         IEnumerable<GrofileUrl> Urls { get; }
+
+        /// <summary>
+        /// Gets a collection of emails associated with this profile.
+        /// </summary>
         IEnumerable<GrofileEmail> Emails { get; }
+
+        /// <summary>
+        /// Gets a collection of photos associated with this profile.
+        /// </summary>
         IEnumerable<GrofilePhoto> Photos { get; }
+
+        /// <summary>
+        /// Gets a collection of accounts associated with this profile.
+        /// </summary>
         IEnumerable<GrofileAccount> Accounts { get; }        
     }
 
