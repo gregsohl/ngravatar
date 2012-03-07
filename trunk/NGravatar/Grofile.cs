cf5e753f44adb25bda6773d989521320e4c6fcf5
kyourek@gmail.com <kyourek@gmail.com@f3f85ded-8c0c-14a0-d2d9-98d2ba1cfc74>
2012-03-07 00:42:40 +0000

    Added documentation for new methods and classes in version 0.3.0.

--- trunk/NGravatar/Grofile.cs
+++ trunk/NGravatar/Grofile.cs
@@ -19,6 +19,9 @@ namespace NGravatar
         }
     }
 
+    /// <summary>
+    /// Class to provide functionality for dealing with Gravatar profile information.
+    /// </summary>
     public class Grofile
     {
         #region Private Members
@@ -59,8 +62,17 @@ namespace NGravatar
         #endregion
 
         #region Public Members
+
+        /// <summary>
+        /// Creates a new instance of the class.
+        /// </summary>
         public Grofile() { }
 
+        /// <summary>
+        /// Gets the URL that links to the Gravatar profile of the specified <paramref name="email"/>.
+        /// </summary>
+        /// <param name="email">The email whose profile should be linked.</param>
+        /// <returns>The URL of the profile for the specified <paramref name="email"/>.</returns>
         public string GetLink(string email)
         {
             if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(email.Trim()))
@@ -69,6 +81,11 @@ namespace NGravatar
             return "http://www.gravatar.com/" + new Gremail(email).Hash();
         }
 
+        /// <summary>
+        /// Parses Gravatar profile information for the specified <paramref name="email"/> into an object.
+        /// </summary>
+        /// <param name="email">The email whose profile information should be returned.</param>
+        /// <returns>An object that contains information about the Gravatar profile for the specified <paramref name="email"/>.</returns>
         public IGrofileInfo GetInfo(string email)
         {
             var xdoc = GetXDocument(email);
@@ -77,6 +94,15 @@ namespace NGravatar
             return new GrofileInfoXml(entry);
         }
 
+        /// <summary>
+        /// Creates a script tag that can be included in an HTML page to process a Gravatar profile on the client.
+        /// </summary>
+        /// <param name="email">The email whose profile should be processed.</param>
+        /// <param name="callback">
+        /// The JavaScript callback function which should be called after the profile information is loaded. The profile
+        /// information will be passed as a paramter to this callback.
+        /// </param>
+        /// <returns>A rendered script tag that can be included in an HTML page.</returns>
         public string RenderScript(string email, string callback)
         {
             var src = GetJsonLink(email, callback);
