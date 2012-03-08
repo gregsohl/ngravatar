6e8751a181a07f4b69479ef3f6a29133f04472d8
kyourek@gmail.com <kyourek@gmail.com@f3f85ded-8c0c-14a0-d2d9-98d2ba1cfc74>
2012-03-08 21:05:03 +0000

    Tagged version 0.3.1.

---
+++ tags/ngravatar.0.3.1/NGravatar/Properties/AssemblyInfo.cs
@@ -0,0 +1,32 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Information about this assembly is defined by the following attributes. 
+// Change them to the values specific to your project.
+
+[assembly: AssemblyTitle("NGravatar")]
+[assembly: AssemblyDescription("NGravatar")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NGravatar")]
+[assembly: AssemblyCopyright("Copyright © Ken Yourek 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: Guid("9D7A2B03-D480-44D8-97CD-3391243E078F")]
+
+// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
+// The form "{Major}.{Minor}.*" will automatically update the build and revision,
+// and "{Major}.{Minor}.{Build}.*" will update just the revision.
+
+[assembly: AssemblyVersion("0.3.1")]
+
+// The following attributes are used to specify the signing key for the assembly, 
+// if desired. See the Mono documentation for more information about signing.
+
+//[assembly: AssemblyDelaySign(false)]
+//[assembly: AssemblyKeyFile("")]
+
+[assembly: ComVisibleAttribute(false)]
+[assembly: InternalsVisibleTo("NGravatar.Tests")]
