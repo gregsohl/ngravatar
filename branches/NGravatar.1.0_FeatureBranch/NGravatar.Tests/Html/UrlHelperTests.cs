2972ec79831ed3cb0adfe400f3eb10098a50c8e8
kyourek@gmail.com <kyourek@gmail.com@f3f85ded-8c0c-14a0-d2d9-98d2ba1cfc74>
2013-07-09 03:28:57 +0000

    Created a branch for version 1.0.

---
+++ branches/NGravatar.1.0_FeatureBranch/NGravatar.Tests/Html/UrlHelperTests.cs
@@ -0,0 +1,43 @@
+using NUnit.Framework;
+using System;
+
+namespace NGravatar.Html.Tests {
+
+    [TestFixture]
+    public class UrlHelperTests {
+
+        [Test]
+        public void GravatarTest() {
+            var size = 274;
+            var email = "ngravatar@kendoll.net";
+            var defaultImage = "pathtodefault.img";
+            var maxRating = Rating.X;
+            var gravatarSrc = UrlHelperExtensions.Gravatar(null, email, size, defaultImage, maxRating);
+
+            var gravatar = new Gravatar();
+            gravatar.Size = size;
+            gravatar.DefaultImage = defaultImage;
+            gravatar.MaxRating = maxRating;
+
+            Assert.AreEqual(gravatar.GetImageSource(email), gravatarSrc);
+        }
+
+        [Test]
+        public void GravatarTest2() {
+            var size = 274;
+            var email = "ngravatar@kendoll.net";
+            var gravatar1 = UrlHelperExtensions.Gravatar(null, email, size, null, null);
+            var gravatar2 = UrlHelperExtensions.Gravatar(null, email, size);
+
+            Assert.AreEqual(gravatar1, gravatar2);
+        }
+
+        [Test]
+        public void GrofileTest() {
+            var email = "some@email.com";
+            var expected = new Grofile().GetLink(email);
+            var actual = UrlHelperExtensions.Grofile(null, email);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
