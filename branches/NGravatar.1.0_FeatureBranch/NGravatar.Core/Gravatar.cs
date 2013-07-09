e3573a360d0bf62f31febafb02c700de67ee2379
kyourek@gmail.com <kyourek@gmail.com@f3f85ded-8c0c-14a0-d2d9-98d2ba1cfc74>
2013-07-09 04:30:39 +0000

    Working on the rewrite for version 1.0.

---
+++ branches/NGravatar.1.0_FeatureBranch/NGravatar.Core/Gravatar.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+using System.Collections.Generic;
+
+namespace NGravatar.Core {
+    public class Gravatar {
+
+        private string GetEmailHash(string email) {
+            if (null == email) throw new ArgumentNullException("email");
+
+            email = email.Trim().ToLower();
+
+            var encoder = new UTF8Encoding();
+            var md5 = new MD5CryptoServiceProvider();
+            var hashedBytes = md5.ComputeHash(encoder.GetBytes(email));
+            var sb = new StringBuilder(hashedBytes.Length * 2);
+
+            for (var i = 0; i < hashedBytes.Length; i++) {
+                sb.Append(hashedBytes[i].ToString("X2"));
+            }
+
+            return sb.ToString().ToLower();
+        }
+
+        /// <summary>
+        /// Gets or sets the maximum Gravatar rating allowed to be displayed
+        /// or <c>null</c> to allow the default Gravatar rating.
+        /// </summary>
+        public GravatarRating? MaxRating {
+            get { return _MaxRating; }
+            set { _MaxRating = value; }
+        }
+        private GravatarRating? _MaxRating;
+
+        /// <summary>
+        /// Gets or sets the image size, in pixels, of the Gravatar images
+        /// or <c>null</c> to use the default Gravatar size.
+        /// </summary>
+        public int? ImageSize {
+            get { return _ImageSize; }
+            set { _ImageSize = value; }
+        }
+        private int? _ImageSize;
+
+        /// <summary>
+        /// Gets or sets the URL of the default image to be shown if no 
+        /// Gravatar is found for an email address.
+        /// </summary>
+        public string DefaultImage { get; set; }
+
+
+    }
+}
---
+++ branches/NGravatar.1.0_FeatureBranch/NGravatar.Core/GravatarRating.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace NGravatar.Core {
+    
+    /// <summary>
+    /// NGravatar avatar rating.
+    /// </summary>
+    public enum GravatarRating {
+    
+        /// <summary>
+        /// G
+        /// </summary>
+        G,
+
+        /// <summary>
+        /// PG
+        /// </summary>
+        PG,
+        
+        /// <summary>
+        /// R
+        /// </summary>
+        R,
+        
+        /// <summary>
+        /// X
+        /// </summary>
+        X
+    }
+}
